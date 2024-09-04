8499153fc1db6ff5f5b187cc9c6684796e551346
RRD4RK <raphaeldegoes2007@gmail.com>
2024-09-04 13:26:21 -0300

    first commit

---
+++ caixa.php
@@ -0,0 +1,10 @@
+<?php
+    
+$logar = readline("Logar-1\n");
+$resgistrar = readline("Registrar-2\n");
+
+
+function logar($logar = "1"){
+    $login = readline("Digite seu nome\n");
+    $senha = readline("Digite sua senha\n");
+}
